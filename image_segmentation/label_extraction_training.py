2ef6dd983f2ad8ac82e67f9196eda97ddea9e4f0
Justine <justinelo010@gmail.com>
2018-11-24 12:19:20 -0500

    changed batch to 100 and iteration to 100

--- image_segmentation/label_extraction_training.py
+++ image_segmentation/label_extraction_training.py
@@ -8,7 +8,6 @@ Created on Fri Nov 23 14:25:42 2018
 from matplotlib import pyplot as plt
 import cv2 # used for resize. if you dont have it, use anything else
 import numpy as np
-import model
 from model import Deeplabv3
 deeplab_model = Deeplabv3()
 #img = plt.imread("imgs/image1.jpg")
@@ -17,10 +16,10 @@ deeplab_model = Deeplabv3()
 L_images = np.load("gray_scale.npy")
 [count, r, c] = L_images.shape
 
-iteration = 20
-batch = 500
+iteration = 100
+batch = 100
 
-for i in range(0, 20):
+for i in range(9, iteration):
     print("ITERATION " + str(i))
     img_L = L_images[i*batch, :, :]
     img = np.stack((img_L, img_L, img_L), axis = 2)
@@ -39,10 +38,10 @@ for i in range(0, 20):
     count_masks_train = np.array([len(set_flat)])
     
     results_train = res
-    label_results_train = np.array([labels])
+    #label_results_train = np.array([labels])
 
     
-    for j in range(i*batch + 1, i*batch+500):
+    for j in range(i*batch + 1, i*batch+batch):
         print("RANGE: " + str(j))
         img_L = L_images[j, :, :]
         img = np.stack((img_L, img_L, img_L), axis = 2)
@@ -62,12 +61,12 @@ for i in range(0, 20):
         
         count_masks_train = np.concatenate((count_masks_train, np.array([len(set_flat)])), axis = 0)
         results_train = np.concatenate((results_train, res), axis = 0)
-        label_results_train = np.concatenate((label_results_train, np.array([labels])), axis = 0)
+        #label_results_train = np.concatenate((label_results_train, np.array([labels])), axis = 0)
     
     
-    np.save("results_train_" + str(i*batch) + "to" + str(i*batch+500) + ".npy", results_train)
-    np.save("count_masks_train_" + str(i*batch) + "to" + str(i*batch+500) + ".npy", count_masks_train)
-    np.save("label_results_train_" + str(i*batch) + "to" + str(i*batch+500) + ".npy", label_results_train)
+    np.save("results_train_" + str(i*batch) + ".npy", results_train)
+    np.save("count_masks_train_" + str(i*batch) + ".npy", count_masks_train)
+    #np.save("label_results_train_" + str(i*batch) + ".npy", label_results_train)
     
     
     
