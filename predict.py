e45adf3e2bd4b5e5a0c5ba7ff42425c478a3b4fa
Ryan <green.ry@husky.neu.edu>
2018-12-07 14:16:13 -0500

    added no fusion predict file

--- predict.py
+++ predict.py
@@ -8,37 +8,41 @@ Created on Sun Nov 25 13:39:36 2018
 
 import numpy as np
 import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+#os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 import tensorflow as tf
 
 from vivid_past_model_definition import VividPastAutoEncoder
 
-from training_utils import  evaluation_pipeline, checkpointing_system
-from save_config import maybe_create_folder
+from training_utils import  evaluation_pipeline, checkpointing_system, maybe_create_folder
 
+factors = np.load('scaling-factors-all-data.npy').item()
 
 # PARAMS
-run_id = 'no_seg_small1'
+run_id = 'no_fusion_3'
+eval_id = 'video_test_1'
 gen_dir = 'eval_predictions'
-sub_folder = 'test_eval'
-num_pred = 10
-begin = 0
-end = begin + num_pred
+sub_folder = run_id + '/' + eval_id
+
+l_array = None
+ab_array = None
 
 sess = tf.Session()
-colorizer = VividPastAutoEncoder(256)
+colorizer = VividPastAutoEncoder(128)
 
-##### LOAD DATA ####
 
-L_channel = np.load("image-colorization/gray_scale.npy", mmap_mode='r')[begin:end, :, :]
-L_channel = ((L_channel / 127.5) -1).astype('float32')
-L_channel = np.expand_dims(L_channel, axis=3)
-AB_channel = np.load("image-colorization/ab/ab1.npy", mmap_mode='r')[begin:end, :, :]
-AB_channel = ((AB_channel / 127.5) -1).astype('float32')
+def norm_data(l, ab):
+    L_channel = ((l  - factors["L_mu"]) / factors["L_factor"] ).astype('float32')
+    L_channel = np.expand_dims(L_channel, axis=3)
+    AB_channel = ((ab  - factors["AB_mu"]) / factors["AB_factor"] ).astype('float32')
+    
+    return L_channel, AB_channel
+
+##### LOAD / NORMALIZE DATA ####
+l, ab = norm_data(l_array, ab_array)
 
 saver, checkpoint_paths, latest_checkpoint = checkpointing_system(run_id)
-evaluation = evaluation_pipeline(colorizer, L_channel, AB_channel)
+evaluation = evaluation_pipeline(colorizer, l, ab)
 
 with sess.as_default():
     # Initialize
@@ -55,7 +59,7 @@ with sess.as_default():
         epoch_cost = res['cost']
         save_dir = os.path.join(gen_dir, sub_folder)
         maybe_create_folder(save_dir)
-        np.save(save_dir + '/' + 'predicted_ab_' + str(begin) + '-' + str(end) + '_cost' + str(res['cost']) , res['predicted_ab'])
+        np.save(save_dir + '/' + 'predicted_ab_' + run_id + '_' + eval_id + '_cost' + str(res['cost']) , res['predicted_ab'])
         print("Predictions saved.")
     
     else:
